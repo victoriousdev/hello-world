48070d4106b77cd40e28a9d1dffce80136429441
Will <me@victoriousdev.com>
2017-10-05 03:59:57 -0400

    added javadoc comment for HelloWorldApp

--- com/victoriousdev/app/helloworld/HelloWorldApp.java
+++ com/victoriousdev/app/helloworld/HelloWorldApp.java
@@ -1,5 +1,12 @@
 package com.victoriousdev.app.helloworld;
 
+/**
+ *
+ * HelloWorldApp
+ * @author victoriousdev
+ *
+ */
+
 class HelloWorldApp {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
